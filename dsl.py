ff3198d100ae406cba98df84f1847a6353df376a
Tim Menzies <tim@menzies.us>
2015-06-20 21:36:23 -0400

    saving

--- dsl.py
+++ dsl.py
@@ -9,8 +9,16 @@ def DSL(): return o(
 def val(x): return x.val() if isa(x,Thing) else  x
 
 class Thing:
-  def __init__(i,txt,init=0):
-    i.val,i.txt = init,txt
+  def __init__(i,txt,model=None,init=0,t=0):
+    i.txt,i.vals,i.model = txt,[(t,init)],model
+  def last(i):
+    for t,val in i.vals:
+      if t >= i.model.now: return val
+    return i.vals[0][1]
+  def now(i,x):
+   t,old = i.vals[0]
+   if old == i.model.now:
+     if 
   def __add__( i, j)  : return i.val() +  val(j)
   def __sub__( i, j)  : return i.val() -  val(j)
   def __mul__( i, j)  : return i.val() +  val(j)
@@ -25,3 +33,7 @@ class Thing:
   def __rdiv__(i, j)  : return j /  i
   def __rmod__(i, j)  : return j // i
   def __rpow__(i, j)  : return j ** i
+  
+
+class Stock(Thing):
+  
